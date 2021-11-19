115c3ece05638f21088331396cfeb145e8314912
anushareddy73 <sunketaanushareddy@gmail.com>
2021-11-18 22:23:36 -0600

    added code

--- app.js
+++ app.js
@@ -3,7 +3,21 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username }, function (err, user) {
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  });
+  }));
 
 const connectionString = 'mongodb+srv://anushareddy6:anusha73@cluster0.kxtz9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' 
 mongoose = require("mongoose");
@@ -70,6 +84,13 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, "public")));
 
 var db = mongoose.connection;
@@ -85,7 +106,13 @@ app.use("/selector", selectorRouter);
 app.use("/addmods", addmodsRouter);
 //app.use("/cherry", cherryRouter);
 app.use('/resource', resourceRouter);
-
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
