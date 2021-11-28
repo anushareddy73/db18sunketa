b28f885e077629490d3d16f2ee54f3cf17bca190
anushareddy73 <sunketaanushareddy@gmail.com>
2021-11-27 20:36:00 -0600

    login done

--- models/cherry.js
+++ models/cherry.js
@@ -1,9 +1,20 @@
-const mongoose = require("mongoose") 
-const cherrySchema = mongoose.Schema({ 
- Cherry_type: String, 
- Weight: Number, 
- Cost: Number 
-}) 
- 
-module.exports = mongoose.model("cherry", 
-cherrySchema) 
+const mongoose = require("mongoose")
+const cherrySchema = mongoose.Schema({
+
+Cherry_type:{
+    type: String,
+    minLength: 3,
+    maxLength: 100
+},
+
+Weight : {
+    type:Number,
+},
+
+Cost: {
+    type:Number,
+    min:1,
+    max:500
+}
+})
+module.exports = mongoose.model("cherry",cherrySchema)
--- routes/cherry.js
+++ routes/cherry.js
@@ -7,12 +7,22 @@ router.get('/', cherry_controlers.cherry_view_all_Page);
 
 /* GET detail cherry page */ 
 router.get('/detail', cherry_controlers.cherry_view_one_Page); 
+// A little function to check if we have an authorized user and continue on
+// or
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user){
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+    }
 /* GET create cherry page */ 
-router.get('/create', cherry_controlers.cherry_create_Page);
+router.get('/create',secured, cherry_controlers.cherry_create_Page);
 /* GET create update page */ 
-router.get('/update', cherry_controlers.cherry_update_Page); 
+router.get('/update',secured, cherry_controlers.cherry_update_Page); 
  /* GET create cherry page */ 
-router.get('/delete', cherry_controlers.cherry_delete_Page); 
+router.get('/delete',secured, cherry_controlers.cherry_delete_Page); 
  
 
 
